884866a825f4d0e51050c85009c3b7ef6a941b44
Goran <g.knabe@campus.tu-berlin.de>
2024-10-07 12:25:08 +0200

    noise

---
+++ Goran_Bachelor_Code/matrixModification.py
@@ -0,0 +1,70 @@
+#In this file I attempt to generate random matrices that are pretty skewed
+import numpy as np
+import Throughput_as_Function as fct
+def generate_doubly_stochastic_matrix(N, min_val=1e-3, max_val=0.9,alpha =0.1, beta=10.0, max_iters=10000, tol=1e-12):#Generates 1-doubly-stochastic skewed matrix of size NxN
+    # Step 1: Initialize random matrix with beta distribution
+    matrix = np.random.beta(alpha, beta, (N, N)) * (max_val - min_val) + min_val
+    np.fill_diagonal(matrix, 0)
+    print(matrix)
+    
+    return(Sinkhorn_Knopp(matrix))
+
+
+def Sinkhorn_Knopp(matrix , max_iters=10000, tol=1e-12):
+    for _ in range(max_iters):
+        # Normalize rows
+        row_sums = matrix.sum(axis=1)
+        matrix = matrix / row_sums[:, np.newaxis]
+        
+        # Normalize columns
+        col_sums = matrix.sum(axis=0)
+        matrix = matrix / col_sums[np.newaxis, :]
+        
+        # Check convergence (small changes in matrix)
+        if np.all(np.abs(matrix.sum(axis=1) - 1) < tol) and np.all(np.abs(matrix.sum(axis=0) - 1) < tol):
+            break
+    
+    return matrix
+
+
+def add_multiplicative_noise(M, N, delta):
+    matrix = np.random.normal(1,delta,(N,N))
+    matrix = np.clip(matrix, 0.1, 2)
+    np.fill_diagonal(matrix, 1)
+    res  = M * matrix
+    print(np.array2string(res,formatter={'float_kind':lambda x: "%.5f" % x}))
+    return M * matrix
+def add_additive_noise(M, N, delta):
+    matrix = np.random.normal(0,delta,(N,N))
+    np.fill_diagonal(matrix, 0)
+    M = np.clip((M+matrix), 0, 100)
+    print(np.array2string(M,formatter={'float_kind':lambda x: "%.5f" % x}))
+    return M
+
+
+if __name__ == "__main__":
+    N = 8
+    matrixdir="/home/studium/Documents/Code/rdcn-throughput/matrices/"
+    M = demandMatrix = np.loadtxt(matrixdir+"permutation-8"+".mat", usecols=range(N))
+
+
+    mult_M = add_multiplicative_noise(M, N, 0.125)
+
+    print("____________________________")
+
+
+    add_M  = add_additive_noise(M, N, 0.05)
+
+    print("____________________________")
+
+    # print(np.array2string(fct.return_normalized_matrix(mult_M),formatter={'float_kind':lambda x: "%.5f" % x}))
+
+    # print(np.array2string(fct.return_normalized_matrix(add_M),formatter={'float_kind':lambda x: "%.5f" % x}))
+
+
+
+    print(np.array2string(Sinkhorn_Knopp(mult_M),formatter={'float_kind':lambda x: "%.5f" % x}))
+
+    print(np.array2string(Sinkhorn_Knopp(add_M),formatter={'float_kind':lambda x: "%.5f" % x}))
+
+    
